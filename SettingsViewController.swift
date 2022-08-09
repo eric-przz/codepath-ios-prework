88e09e165c7684754616a496a8959e01ad5213cc
Eric <eric.prz10@gmail.com>
2022-08-09 02:03:45 -0500

    added prework files to repo

---
+++ SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  prework-tip-app
+//
+//  Created by Eric Perez on 8/3/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var Tip1TextField: UITextField!
+    @IBOutlet weak var Tip2TextField: UITextField!
+    @IBOutlet weak var Tip3TextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
